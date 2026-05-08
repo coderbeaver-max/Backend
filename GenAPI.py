96f9d96ebec83ce36619b31016dc43aad562e085
Mukesh <emutrrx@gmail.com>
2026-05-08 13:20:50 +0530

    after adding the git package in requirements

--- GenAPI.py
+++ GenAPI.py
@@ -3,6 +3,7 @@ from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
+
 import google.generativeai as genai
 from dotenv import load_dotenv
 
